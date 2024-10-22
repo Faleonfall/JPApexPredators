2d49c3054ffb4dd98fbe465d83e2d4fa88e8d364
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-10-22 17:55:46 +0200

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Volodymyr Kryvytskyi on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
