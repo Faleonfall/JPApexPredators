c0ede5cdfeb56739f9f79b0967617a4c28d0439f
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-10-23 18:01:29 +0200

    Added a data model and assets

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,25 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Volodymyr Kryvytskyi on 22.10.2024.
+//
+
+import Foundation
+
+struct ApexPredator: Decodable {
+    let id: Int
+    let name: String
+    let type: String
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    struct MovieScene: Decodable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
